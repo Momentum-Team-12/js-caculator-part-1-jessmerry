7bc8df2de7050cc30d4729d2757f7a3735c8e73c
Jess Merry <merryja.email@gmail.com>
2022-04-01 12:56:02 -0400

    button click

---
+++ main.js
@@ -0,0 +1,13 @@
+// let squares = document.querySelectorAll('.square')
+//     for (let square of squares){
+//         square.addEventListener('click', function (){
+//             console.log("button was pressed")
+//             // console.log(event.target.innerText)
+//             // event.target.innerText = "clicked"
+//         })
+//     }
+
+let key1 = document.getElementById("square-one")
+key1.addEventListener('click', function (){
+    console.log("button was pressed")
+})
