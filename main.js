7b046b4b381e5fc80aaade9fa916bf5e3d2e1e10
Jess Merry <merryja.email@gmail.com>
2022-04-03 14:54:04 -0400

    math

--- main.js
+++ main.js
@@ -6,13 +6,30 @@ console.log ("Hello, world!")
 const results = document.getElementById("results")
 console.log ("results div", results);
 
+const equals = document.getElementById("square-equals")
+
+const clear = document.getElementById("square-clear")
+
 let squares = document.querySelectorAll('.square')
+
     for (let square of squares){
         square.addEventListener('click', function (event){
-            console.log(event.target.innerText)
+            if (event.target === clear){
+                results.innerText = ""
+            }
+            else if (event.target === equals){
+                results.innerText = eval(results.innerText)
+            }
+            else {
+                console.log(event.target.innerText)
+                results.innerText += event.target.innerText
+            }
         })
     }
 
+// if we click a number, return the number - else
+// if we click clear, empty the results field - if .
+// if we click equals, return answer - else if .
 
 // let key1 = document.getElementById("square-one")
 // key1.addEventListener('click', function (){
