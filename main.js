2cb569b2222d0a35e9b746dc486eefab229a3211
Jess Merry <merryja.email@gmail.com>
2022-04-03 12:36:05 -0400

    update css and shadows

--- main.js
+++ main.js
@@ -1,13 +1,20 @@
-// let squares = document.querySelectorAll('.square')
-//     for (let square of squares){
-//         square.addEventListener('click', function (){
-//             console.log("button was pressed")
-//             // console.log(event.target.innerText)
-//             // event.target.innerText = "clicked"
-//         })
-//     }
+console.log ("Hello, world!")
 
-let key1 = document.getElementById("square-one")
-key1.addEventListener('click', function (){
-    console.log("button was pressed")
-})
+// const numSquares = document.querySelectorAll(".square");
+// console.log ("array of squares", numSquares);
+
+const results = document.getElementById("results")
+console.log ("results div", results);
+
+let squares = document.querySelectorAll('.square')
+    for (let square of squares){
+        square.addEventListener('click', function (event){
+            console.log(event.target.innerText)
+        })
+    }
+
+
+// let key1 = document.getElementById("square-one")
+// key1.addEventListener('click', function (){
+//     console.log("button was pressed")
+// })
